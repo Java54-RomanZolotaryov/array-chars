2c74a40f9c009a37dd884eaf29de11f8cf2e4cd0
Roman <romanzolotdev@gmail.com>
2024-03-07 19:20:28 +0200

    init commit

---
+++ src/telran/arrays/ArrayChar.java
@@ -0,0 +1,15 @@
+package telran.arrays;
+
+public class ArrayChar {
+	private char[] array;
+	
+	public ArrayChar(char[] array) {
+		this.array = array;
+	}
+	
+	public int compareTo(ArrayChar other) {
+		//TODO
+		return -1;
+	}
+	
+}
---
+++ src/telran/arrays/test/ArrayCharTest.java
@@ -0,0 +1,26 @@
+package telran.arrays.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.arrays.ArrayChar;
+
+class ArrayCharTest {
+
+	@Test
+	void testCompareTo() {
+		char[] arr1 = {'1', '2', '3'};
+		char[] arr2 = {'9'};
+		char[] arr3 = {'1', '2', '3'};
+		ArrayChar arrayChar1 = new ArrayChar(arr1);
+		ArrayChar arrayChar2 = new ArrayChar(arr2);
+		ArrayChar arrayChar3 = new ArrayChar(arr3);
+		assertEquals(8, arrayChar2.compareTo(arrayChar1));
+		assertEquals(-8, arrayChar1.compareTo(arrayChar2));
+		assertEquals(1, new ArrayChar(new char[] {'a', 'a'})
+				.compareTo(new ArrayChar(new char[] {'a'})));
+		assertEquals(0, arrayChar1.compareTo(arrayChar3));
+	}
+
+}
