ea35b646d2b9ad5543f12168eef6c4d01811447d
Roman <romanzolotdev@gmail.com>
2024-03-10 09:29:09 +0200

    HW#05 commit

--- src/telran/arrays/ArrayChar.java
+++ src/telran/arrays/ArrayChar.java
@@ -33,4 +33,38 @@ public class ArrayChar {
 		return index;
 	}
 	
+	public int count(char character) {
+		int count = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == character) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public int compareToIgnoreCase(ArrayChar other) {
+		int length = Math.min(this.array.length, other.array.length);
+		int index = 0;
+		
+		//We use bitwise operator '^(XOR)' to compare characters with case ignored
+		//However only works in scope of ASCII Table codes
+		while(index < length && (this.array[index] == other.array[index] || this.array[index] == (other.array[index] ^ 32))) {
+			index++;
+		}
+		return index == length ? this.array.length - other.array.length :
+			this.array[index] - other.array[index];
+	}
+	
+	public boolean contains(char character) {
+		return indexOf(character) != -1;
+	}
+	
+	public boolean equals(ArrayChar other) {
+		return compareTo(other) == 0;
+	}
+	
+	public boolean equalsIgnoreCase(ArrayChar other) {
+		return compareToIgnoreCase(other) == 0;
+	}	
 }
--- src/telran/arrays/test/ArrayCharTest.java
+++ src/telran/arrays/test/ArrayCharTest.java
@@ -38,5 +38,62 @@ class ArrayCharTest {
 		assertEquals(3, arrayChar.lastIndexOf('2'));
 		assertEquals(-1, arrayChar.indexOf('9'));
 	}
+	
+	@Test
+	void testCount() {
+		char[] arr1 = {'1', '2', '3', '2'};
+		ArrayChar arrayChar = new ArrayChar(arr1);
+		assertEquals(2, arrayChar.count('2'));
+		assertEquals(1, arrayChar.count('1'));
+		assertEquals(0, arrayChar.count('9'));
+	}
+	
+	@Test
+	void testCompareToIgnoreCase() {
+		char[] arr1 = {'h','e','l','l'};
+		char[] arr2 = {'H','E','L','l'};
+		char[] arr3 = {'H','e'};
+		ArrayChar arrayChar1 = new ArrayChar(arr1);
+		ArrayChar arrayChar2 = new ArrayChar(arr2);
+		ArrayChar arrayChar3 = new ArrayChar(arr3);
+		assertEquals(0, arrayChar1.compareToIgnoreCase(arrayChar2));
+		assertEquals(2, arrayChar2.compareToIgnoreCase(arrayChar3));
+		assertEquals(2, arrayChar1.compareToIgnoreCase(arrayChar3));
+		assertEquals(-2, new ArrayChar(new char[0]).compareToIgnoreCase(arrayChar3));
+	}
+	
+	@Test
+	void containsTest() {
+		char[] arr = {'h','E','L','l'};
+		ArrayChar arrayChar = new ArrayChar(arr);
+		assertTrue(arrayChar.contains('h'));
+		assertTrue(arrayChar.contains('E'));
+		assertFalse(arrayChar.contains('e'));
+		assertFalse(arrayChar.contains('\0'));
+	}
+	
+	@Test
+	void equalsTest() {
+		char[] arr1 = {'h','E','L','l'};
+		char[] arr2 = {'h','E','L','l'};
+		char[] arr3 = {'h','e','L'};
+		ArrayChar arrayChar1 = new ArrayChar(arr1);
+		ArrayChar arrayChar2 = new ArrayChar(arr2);
+		ArrayChar arrayChar3 = new ArrayChar(arr3);
+		assertTrue(arrayChar2.equals(arrayChar1));
+		assertFalse(arrayChar3.equals(arrayChar1));
+	}
+	
+	@Test
+	void equalsIgnoreCase() {
+		char[] arr1 = {'H','E','l','L'};
+		char[] arr2 = {'h','e','L','l'};
+		char[] arr3 = {'h','e','L','o'};
+		ArrayChar arrayChar1 = new ArrayChar(arr1);
+		ArrayChar arrayChar2 = new ArrayChar(arr2);
+		ArrayChar arrayChar3 = new ArrayChar(arr3);
+		assertTrue(arrayChar1.equalsIgnoreCase(arrayChar2));
+		assertFalse(arrayChar3.equalsIgnoreCase(arrayChar2));
+	}
 
 }
