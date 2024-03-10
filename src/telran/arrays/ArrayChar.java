a5cec85560119682d0c9be4ce35bca8ad6fcd21a
Roman <romanzolotdev@gmail.com>
2024-03-10 08:25:30 +0200

    Complete CW#05 commit

--- src/telran/arrays/ArrayChar.java
+++ src/telran/arrays/ArrayChar.java
@@ -8,8 +8,29 @@ public class ArrayChar {
 	}
 	
 	public int compareTo(ArrayChar other) {
-		//TODO
-		return -1;
+		int length = Math.min(this.array.length, other.array.length);
+		int index = 0;
+		while(index < length && this.array[index] == other.array[index]) {
+			index++;
+		}
+		return index == length ? this.array.length - other.array.length :
+			this.array[index] - other.array[index];
+	}
+	
+	public int indexOf(char character) {
+		int index = 0;
+		while(index < array.length && array[index] != character) {
+			index++;
+		}
+		return index == array.length ? -1 : index;
+	}
+	
+	public int lastIndexOf(char character) {
+		int index = array.length - 1;
+		while (index > -1 && array[index] != character) {
+			index--;
+		}
+		return index;
 	}
 	
 }
--- src/telran/arrays/test/ArrayCharTest.java
+++ src/telran/arrays/test/ArrayCharTest.java
@@ -22,5 +22,21 @@ class ArrayCharTest {
 				.compareTo(new ArrayChar(new char[] {'a'})));
 		assertEquals(0, arrayChar1.compareTo(arrayChar3));
 	}
+	
+	@Test
+	void indexOfTest() {
+		char[] arr1 = {'1', '2', '3', '2'};
+		ArrayChar arrayChar = new ArrayChar(arr1);
+		assertEquals(1, arrayChar.indexOf('2'));
+		assertEquals(-1, arrayChar.indexOf('9'));
+	}
+	
+	@Test
+	void lastIndexOfTest() {
+		char[] arr1 = {'1', '2', '3', '2'};
+		ArrayChar arrayChar = new ArrayChar(arr1);
+		assertEquals(3, arrayChar.lastIndexOf('2'));
+		assertEquals(-1, arrayChar.indexOf('9'));
+	}
 
 }
